94c7a3112359d1b8c0e325a8cd8658799e8d7516
David Cook <divergentdave@gmail.com>
2013-07-16 18:16:19 -0500

    Bump version string

--- plugins/keys-import.user.js
+++ plugins/keys-import.user.js
@@ -59,7 +59,7 @@ window.plugin.importkeys.openDialog = function(event) {
     return;
   }
   var handshakeUrl = window.plugin.importkeys.APPSPOT_URL + '/handshake?json='
-    + encodeURIComponent(window.JSON.stringify({'nemesisSoftwareVersion': '2013-06-28T23:28:27Z 760a7a8ffc90 opt', 'deviceSoftwareVersion': '4.1.1'}));
+    + encodeURIComponent(window.JSON.stringify({'nemesisSoftwareVersion': '2013-07-12T15:48:09Z d6f04b1fab4f opt', 'deviceSoftwareVersion': '4.1.1'}));
   var div = document.createElement('div');
   var span = document.createElement('span');
   span.appendChild(document.createTextNode('Log in below to import your Ingress inventory. (Or try again in a '));
